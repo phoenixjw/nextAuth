2d1b6ec03f7e8b620715170d73b69366d327fe72
phoenixjw <phoenixjw9320@gmail.com>
2023-07-17 12:24:57 +0100

    simple pages for login, signup, profile

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,65 @@
+'use client'
 
+import Link from 'next/link';
+import React from 'react';
+import { useRouter } from "next/navigation" 
+import { axios } from "axios"
 
-const Signup = () => {
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  })
+
+  const onSignUp = async() => {
+    
+  }
+
   return (
-    <div>Signup</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>SignUp</h1>
+      <hr/>
+      <label htmlFor='username'>username</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        id="username"
+        type='text'
+        value={user.username}
+        onChange={(e) => setUser({...user, username: e.target.value})}
+        placeholder='username'
+      
+      />
+
+      <label htmlFor='email'>email</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        id="email"
+        type='text'
+        value={user.email}
+        onChange={(e) => setUser({...user, email: e.target.value})}
+        placeholder='email'
+      
+      />
+
+      <label htmlFor='password'>password</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        id="password"
+        type='password'
+        value={user.password}
+        onChange={(e) => setUser({...user, password: e.target.value})}
+        placeholder='password'
+      />
+
+      <button 
+        onClick={onSignUp}
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+      >
+        Signup here
+      </button>
+      <Link href='/login'>Visit Login page</Link>
+    </div>
   )
 }
 
-export default Signup
